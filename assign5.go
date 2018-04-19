e278fc528a21b3a38ab787a8abf74affbb63a0d1
SaumyashreeRay <saumyaray1@gmail.com>
2018-04-19 17:22:28 -0400

    Add files via upload

--- assign5.go
+++ assign5.go
@@ -1,88 +1,100 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "log"
-  "net"
+    "bufio"
+    "fmt"
+    "log"
+    "net"
+    "time"
 )
 
 type client chan<- string // an outgoing message channel
 
 var (
-  entering = make(chan client)
-  leaving  = make(chan client)
-  messages = make(chan string) // all incoming client messages
+    entering = make(chan client)
+    leaving  = make(chan client)
+    messages = make(chan string) // all incoming client messages
 )
 
-// func echo(c net.Conn, shout string) {
-//   fmt.Fprintln(c, shout)
-// }
-
 func broadcaster() {
-	clients := make(map[client]bool) // all connected clients
-  	for {
-		select {
-		// 1. Send the message to all the clients
-			case msg := <-messages:
-		// 2. Update the clients map
-			case cli := <-entering:
-		// 3. Update the clients map and close the client channel
-			case cli := <-leaving:
-		}
-  	}
+      clients := make(map[client]bool) // all connected clients
+      for {
+          select {
+          
+          case msg := <-messages:
+            // 1. Send the message to all the clients
+                for cli:=range clients{
+                    cli <-msg
+                }
+          case cli := <-entering:
+            // 2. Update the clients map
+                clients[cli]=true
+          case cli := <-leaving:
+            // 3. Update the clients map and close the client channel
+                delete(clients,cli)
+                close(cli)
+          }
+      }
 }
 
 func handleConn(conn net.Conn) {
-	ch := make(chan string) // outgoing client messages
-	go clientWriter(conn, ch)
+    ch := make(chan string) // outgoing client messages
+    go clientWriter(conn, ch)
 
-  // Client's IP address
-  	who := conn.RemoteAddr().String()
+    timer1 := time.NewTimer(5 * time.Second)
 
-  // 1. Send a message to the new client confirming their identity
-  // e.g. "You are 1.2.3"
+    // Client's IP address
+    who := conn.RemoteAddr().String()
 
-  // 2. Broadcast a message to all the clients that a new client has joined
-  // e.g. "1.2.3. has joined"
+    // 1. Send a message to the new client confirming their identity
+    // e.g. "You are 1.2.3"
+    ch <- "You are " + who
 
-  // 3. Send the client to the entering channel
+    // 2. Broadcast a message to all the clients that a new client has joined
+    // e.g. "1.2.3. has joined"
+    messages <- who + " has joined"
 
-  // 4. Use a scanner (e.g. bufio.NewScanner) to read from the client and broadcast the incoming messages to all the clients
+    // 3. Send the client to the entering channel
+    entering <- ch
 
-  // 5. Send the client to the leaving channel
+    // 4. Use a scanner (e.g. bufio.NewScanner) to read from the 
+    //client and broadcast the incoming messages to all the clients
+    input := bufio.NewScanner(conn)
+    for input.Scan() {
+        //broadcast message to all other clients
+        messages <- who+":"+input.Text()
+    }
 
-  // 6. Broadcast a message to all clients that the client has left
-  // e.g. "1.2.3. has left"
+    conn.Close()
 
-	conn.Close()
+    // 5. Send the client to the leaving channel
+    leaving<-ch
+
+    // 6. Broadcast a message to all clients that the client has left
+    // e.g. "1.2.3. has left"
+    messages<- who+" has left"
+    conn.Close()
 }
 
 func clientWriter(conn net.Conn, ch <-chan string) {
-	for msg := range ch {
-    		fmt.Fprintln(conn, msg)
-  	}
+    for msg := range ch {
+        fmt.Fprintln(conn, msg)
+    }
 }
 
-// func handleConn(c net.Conn) {
-//   input := bufio.NewScanner(c)
-//   for input.Scan() {
-//     echo(c, "Message received: " + input.Text())
-//   }
-//   c.Close()
-// }
-
 func main() {
-  l, err := net.Listen("tcp", "localhost:8000")
-  if err != nil {
-    log.Fatal(err)
-  }
-  for {
-    conn, err := l.Accept()
+    listener, err := net.Listen("tcp", "localhost:8000")
     if err != nil {
-      log.Print(err)
-      continue
+        log.Fatal(err)
     }
-    handleConn(conn)
-  }
+
+    go broadcaster()
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            log.Print(err)
+            continue
+        }
+        go handleConn(conn)
+    }
 }
